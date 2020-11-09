340066ffefa3f4c96289a82fe8f82933e93920bb
Joseph Bagaric <bagaricjos@gmail.com>
2020-11-09 12:26:14 +0100

    chore: disable the I-REC feature + fixate deps

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -6,10 +6,12 @@ import {
     OriginConfigurationProvider,
     createOriginConfiguration
 } from '@energyweb/origin-ui-core';
+import { allOriginFeatures, OriginFeature } from '@energyweb/utils-general';
 
 import './styles/app.scss';
 
 const originConfiguration = createOriginConfiguration({
+    enabledFeatures: allOriginFeatures.filter(feature => feature !== OriginFeature.IRec && feature !== OriginFeature.IRecConnect)
     /* 
         Override the Origin configuration here
     */
