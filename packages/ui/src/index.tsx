0fd2a3a90cc5009d621e084b65df0fbae83ce509
Mateusz Koteja <mateusz.koteja@leocode.com>
2021-04-20 13:15:37 +0200

    chore(ui): make UI starting (raw webpack config)

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,25 +1,10 @@
 import { render } from 'react-dom';
 import React from 'react';
-
-import {
-    Origin,
-    OriginConfigurationProvider,
-    createOriginConfiguration
-} from '@energyweb/origin-ui-core';
-import { allOriginFeatures, OriginFeature } from '@energyweb/utils-general';
-
 import './styles/app.scss';
 
-const originConfiguration = createOriginConfiguration({
-    enabledFeatures: allOriginFeatures.filter(feature => feature !== OriginFeature.IRec && feature !== OriginFeature.IRecConnect)
-    /* 
-        Override the Origin configuration here
-    */
-});
-
 render(
-    <OriginConfigurationProvider value={originConfiguration}>
-        <Origin />
-    </OriginConfigurationProvider>,
+    <div>
+        Origin Integration Example
+    </div>,
     document.getElementById('root')
 );
